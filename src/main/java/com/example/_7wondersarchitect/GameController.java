e60388c5a6e267ba28ebf0648c9be11d9861e739
Alex <alexandre.silveira24@gmail.com>
2023-01-27 09:25:56 +0100

    hola

--- src/main/java/com/example/_7wondersarchitect/GameController.java
+++ src/main/java/com/example/_7wondersarchitect/GameController.java
@@ -24,7 +24,7 @@ import static com.example._7wondersarchitect.HelloController.*;
 public class GameController {
 
     @FXML
-    private ImageView cartewonder1, cartewonder2;
+    private ImageView cartewonder1, cartewonder2, cartewonder3, cartewonder4, cartewonder5, cartewonder6, cartewonder7, cartewonder8, cartewonder9, cartewonder10, cartewonder11, cartewonder12, cartewonder13, cartewonder14, cartewonder15, cartewonder16, cartewonder17, cartewonder18, cartewonder19, cartewonder20, cartewonder21, cartewonder22, cartewonder23, cartewonder24;
     @FXML
     private  ImageView alexandrieWonder1, babyloneWonder1, epheseWonder1, gizehWonder1, halicarnasseWonder1, olympieWonder1, rhodesWonder1;
     @FXML
@@ -116,6 +116,184 @@ public class GameController {
         cartewonder2.getTransforms().add(rotate);
         transition.play();
     }
+    public void deplacementCarte3(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder3);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder3.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte4(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder4);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder4.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte5(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder5);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder5.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte6(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder6);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder6.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte7(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder7);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder7.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte8(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder8);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder8.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte9(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder9);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder9.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte10(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder10);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder10.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte11(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder11);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder11.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte12(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder12);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder12.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte13(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder13);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder13.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte14(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder14);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder14.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte15(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder15);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder15.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte16(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder16);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder16.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte17(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder17);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder17.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte18(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder18);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder18.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte19(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder19);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder19.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte20(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder20);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder20.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte21(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder21);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder21.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte22(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder22);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder22.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte23(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder23);
+        transition.setToX(263);
+        transition.setToY(0);
+        Rotate rotate = new Rotate(0);
+        cartewonder23.getTransforms().add(rotate);
+        transition.play();
+    }
+    public void deplacementCarte24(MouseEvent mouseEvent) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), cartewonder24);
+        transition.setToX(-307);
+        transition.setToY(-99);
+        Rotate rotate = new Rotate(180);
+        cartewonder24.getTransforms().add(rotate);
+        transition.play();
+    }
+
+
 
 
 }
